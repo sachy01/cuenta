973cc516744c1fbef820d3dcfce0075b3813aff0
JonathanVSB <105444770+JonathanVSB@users.noreply.github.com>
2022-05-25 23:15:42 +0200

    Add files via upload

---
+++ TrabajoM03/src/trabajom03/Usuario.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabajom03;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author ASIX1
+ */
+public class Usuario implements Serializable{
+    
+    public String nombre;
+    public String contraseña;
+    public String rol;
+    
+    
+    
+}
